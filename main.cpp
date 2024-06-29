743ba3dfff153be4f0adbc090f4792b3cb7ee66b
RaKalik963 <andrejmahot646@gmail.com>
2024-06-29 18:07:37 +0300

    sdsd

--- main.cpp
+++ main.cpp
@@ -1,11 +1,117 @@
 #include <iostream>
 using namespace std;
 
+int countOccurrences(const char* text, const char* word) {
+    int count = 0;
+    int textLength = 0;
+    int wordLength = 0;
 
+    while (text[textLength] != '\0') {
+        textLength++;
+    }
+    while (word[wordLength] != '\0') {
+        wordLength++;
+    }
 
-int main()
-{
+    for (int i = 0; i <= textLength - wordLength; i++) {
+        bool found = true;
+        for (int j = 0; j < wordLength; j++) {
+            if (text[i + j] != word[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            count++;
+        }
+    }
 
-	system("pause");
-	return 0;
+    return count;
+}
+
+int countSentences(const char* text) {
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            count++;
+        }
+    }
+    return count;
+}
+
+void countDotsAndCommas(const char* text, int& dots, int& commas) {
+    dots = 0;
+    commas = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == '.') {
+            dots++;
+        }
+        else if (text[i] == ',') {
+            commas++;
+        }
+    }
+}
+
+void reverseString(char* text) {
+    int length = 0;
+    while (text[length] != '\0') {
+        length++;
+    }
+
+    for (int i = 0; i < length / 2; i++) {
+        char temp = text[i];
+        text[i] = text[length - 1 - i];
+        text[length - 1 - i] = temp;
+    }
+}
+
+void reverseSentences(char* text) {
+    int start = 0;
+    int length = 0;
+
+    while (text[length] != '\0') {
+        length++;
+    }
+
+    for (int i = 0; i <= length; i++) {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?' || text[i] == '\0') {
+            int end = i - 1;
+            while (start < end) {
+                char temp = text[start];
+                text[start] = text[end];
+                text[end] = temp;
+                start++;
+                end--;
+            }
+            start = i + 1;
+        }
+    }
+}
+
+int main() {
+    const char text[] = "Count the number of occurrences of a word specified by the user in the text. Turn the entire text over.";
+    cout << text << endl;
+    char word[100];  
+    cout << "Enter word: ";
+    cin >> word;
+
+    int occurrences = countOccurrences(text, word);
+    cout << "Occurrences of \"" << word << "\": " << occurrences << endl;
+
+    int sentences = countSentences(text);
+    cout << "Number of sentences: " << sentences << endl;
+
+    int dots, commas;
+    countDotsAndCommas(text, dots, commas);
+    cout << "Dots: " << dots << ", Commas: " << commas << endl;
+
+    char reversedText[] = "Count the number of, occurrences of, a word specified by the, user in the text. Turn the entire text over.";
+    reverseString(reversedText);
+    cout << "Reversed text: " << reversedText << endl;
+
+    char reversedSentencesText[] = "Count the number of, occurrences of, a word specified by the, user in the text. Turn the entire text over.";
+    reverseSentences(reversedSentencesText);
+    cout << "Reversed sentences text: " << reversedSentencesText << endl;
+
+    return 0;
 }
